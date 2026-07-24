d5df1d5af3e6df35afd6d246fbf243a9ad741d8d
doronm <doronm@chain-reaction.io>
2026-07-24 14:45:46 +0300

    EX_4

--- Express-Intro-&-routes/EX_2/server.js
+++ Express-Intro-&-routes/EX_2/server.js
@@ -1,9 +1,26 @@
 const express = require(`express`);
 const app = express();
 
+const store = [
+    { name: "table", inventory: 3, price: 800 },
+    { name: "chair", inventory: 16, price: 120 },
+    { name: "couch", inventory: 1, price: 1200 },
+    { name: "picture frame", inventory: 31, price: 70 }
+]
+
 const port = 3000;
 app.get("/", (req, res) => {
-  res.send("Server is up and running smoothly");
+  res.send("Hi from my server");
+});
+
+app.get("/priceCheck/:name", (req, res) => {
+  const itemNmae = req.params.name.toLowerCase();
+  const item = store.find(item => item.name.toLowerCase() === itemNmae);
+
+  if (!item){
+    return res.status(404).send("Item not found");
+  }
+  res.send({price:item.price})
 
 });
 
